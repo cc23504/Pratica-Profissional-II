1f2d7926e4e86f0cd5d7f6aac0887e234a75511e
Carolina Calinca <142462838+cc23507@users.noreply.github.com>
2023-10-19 01:23:51 +0000

    atualizando o bd

--- SQLquery.sql
+++ SQLquery.sql
@@ -3,17 +3,17 @@ create schema Carregaytor
 create table Carregaytor.Cliente (
 	id_usuario INT IDENTITY,
 	cpf_usuario INT NOT NULL,
-	nome_usuario NOT NULL,
-	telefone_usuario NOT NULL,
-	email_usuario NOT NULL,
-	senha_usuario NOT NULL,
+	nome_usuario varchar(40) NOT NULL,
+	telefone_usuario char(12) NOT NULL,
+	email_usuario varchar(40) NOT NULL,
+	senha_usuario varchar(20) NOT NULL,
 	CONSTRAINT PK_Usuario PRIMARY KEY (id_usuario)
 )
 
 create table Carregaytor.Carregadores(
-	cod_prod INT INDENTITY,
-	marca_prod NOT NULL,
-	tipo_prod NOT NULL,
+	cod_prod INT IDENTITY,
+	marca_prod varchar(15) NOT NULL,
+	tipo_prod varchar(15) NOT NULL,
 	CONSTRAINT PK_Prod PRIMARY KEY (cod_prod)
 )
 
@@ -28,11 +28,11 @@ create table Carregaytor.Armario(
 
 create table Carregaytor.Pedido (
     id_pedido INT IDENTITY,
-    id_usuario NOT NULL,
-    id_lock NOT NULL,
-	status_pedido NOT NULL,
+    id_usuario int NOT NULL,
+    id_lock int NOT NULL,
+	status_pedido char(5) NOT NULL,
 	horario_inicio date NOT NULL,
-	horario_final date NOT NULL,e
+	horario_final date NOT NULL,
     CONSTRAINT PK_pedido PRIMARY KEY (id_pedido),
     CONSTRAINT FK_id_usuario FOREIGN KEY (id_usuario)
 	    REFERENCES Carregaytor.Cliente(id_usuario),
