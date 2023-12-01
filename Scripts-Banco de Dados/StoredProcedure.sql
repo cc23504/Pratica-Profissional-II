5a4ba1ec56172dc78a154637cedb821a56341dd3
cc23619 <cc23619@g.unicamp.br>
2023-12-01 00:28:46 -0300

    atualizações de Stored Procedure, ClientesDao e att de chave única

--- Scripts-Banco de Dados/StoredProcedure.sql
+++ Scripts-Banco de Dados/StoredProcedure.sql
@@ -1,3 +1,5 @@
+-- CÓDIGO ATUALIZADO PARA VERIFICAR A DUPLICAÇÃO DE CPF, EMAIL E TELEFONE NO BANCO DE DADOS
+
 CREATE PROCEDURE InserirCliente
     @NomeCliente VARCHAR(40),
     @CPFCliente VARCHAR(20),
@@ -9,33 +11,58 @@ BEGIN
     DECLARE @vCodCliente INT;
     DECLARE @vExisteCPF INT;
     DECLARE @vExisteEmail INT;
-
+    DECLARE @vExisteTelefone INT;
 
     SELECT @vExisteCPF = COUNT(*) FROM ChargerHelp.Cliente WHERE cpf_cliente = @CPFCliente;
 
-
     SELECT @vExisteEmail = COUNT(*) FROM ChargerHelp.Cliente WHERE email_cliente = @EmailCliente;
 
+    SELECT @vExisteTelefone = COUNT(*) FROM ChargerHelp.Cliente WHERE telefone_cliente = @TelefoneCliente;
 
     IF @vExisteCPF > 0
     BEGIN
         SELECT 'CPF já existe! Cadastro não realizado' AS retorno;
     END
- 
     ELSE IF @vExisteEmail > 0
     BEGIN
         SELECT 'E-mail já existe! Cadastro não realizado' AS retorno;
     END
+    ELSE IF @vExisteTelefone > 0
+    BEGIN
+        SELECT 'Telefone já existe! Cadastro não realizado' AS retorno;
+    END
     ELSE 
     BEGIN
-     
         SELECT @vCodCliente = ISNULL(MAX(cod_cliente), 0) + 1 FROM ChargerHelp.Cliente;
 
         INSERT INTO ChargerHelp.Cliente (cod_cliente, cpf_cliente, nome_cliente, telefone_cliente, email_cliente, senha_cliente)
         VALUES (@vCodCliente, @CPFCliente, @NomeCliente, @TelefoneCliente, @EmailCliente, @SenhaCliente);
 
-    
         SELECT @vCodCliente AS retorno;
     END
 END;
 GO
+
+-- ************* VERIFICAÇÃO DA STORED PROCEDURE ****************** 
+-- (MOSTRAR O DUNCIONAMENTO PARA O PROFESSOR NA APRESENTAÇÃO)
+
+-- COLOCAR OS DADOS DE UM CLIENTE JÁ INSERIDO NO BANCO DE DADOS E TAMBÉM DE UM NOVO CLIENTE
+
+-- DECLARE @retorno INT;
+
+-- -- Teste de inserção de um novo cliente
+-- EXEC InserirCliente 
+--     @NomeCliente = 'NOME',
+--     @CPFCliente = 'CPF',
+--     @TelefoneCliente = 'TELEFONE', 
+--     @EmailCliente = 'EMAIL', 
+--     @SenhaCliente = 'SENHA';
+
+-- -- Captura do valor retornado pela stored procedure
+-- SELECT @retorno = SCOPE_IDENTITY();
+
+-- -- Exibição do resultado do teste
+-- IF @retorno IS NOT NULL AND @retorno > 0
+--     PRINT 'Novo cliente inserido com sucesso! Código do cliente: ' + CAST(@retorno AS VARCHAR(10));
+-- ELSE
+--     PRINT 'Erro ao inserir o cliente.';
--- app/bd/clientesDao.js
+++ app/bd/clientesDao.js
@@ -2,63 +2,63 @@ class ClientesDao {
     constructor(bd) {
         this._bd = bd;
     }
-
-    inserirClienteNoBanco(nome, email, cpf, telefone, senha) {
-        return new Promise((resolve, reject) => {
-            const storedProcedure = 'InserirCliente';
-            
-            // Define os parâmetros usando a notação @param
-            const params = {
-                NomeCliente: nome,
-                CPFCliente: cpf,
-                TelefoneCliente: telefone,
-                EmailCliente: email,
-                SenhaCliente: senha
-            };
     
-            // Chama a stored procedure com os parâmetros
-            this._bd.request()
-                .input('NomeCliente', mssql.VarChar, params.NomeCliente)
-                .input('CPFCliente', mssql.VarChar, params.CPFCliente)
-                .input('TelefoneCliente', mssql.VarChar, params.TelefoneCliente)
-                .input('EmailCliente', mssql.VarChar, params.EmailCliente)
-                .input('SenhaCliente', mssql.VarChar, params.SenhaCliente)
-                .execute(storedProcedure, (err, result) => {
-                    if (err) {
-                        console.log(err);
-                        return reject("CADASTRO DE CLIENTE FALHOU!");
-                    }
-                    resolve(result);
-                });
-        });
+    async inserirClienteNoBanco(nome, email, cpf, telefone, senha) {
+        try {
+            // Verificação de e-mail existente
+            const clientes = await this.buscarClientePorEmail(email);
+            if (clientes.length > 0) {
+                throw new Error("E-mail já existe! Cadastro não realizado.");
+            }
+
+            // Verificação de CPF existente
+            const recordsetCPF = await this.queryDB(`SELECT * FROM ChargerHelp.Cliente WHERE cpf_cliente = '${cpf}'`);
+            if (recordsetCPF.length > 0) {
+                throw new Error("CPF já existe! Cadastro não realizado.");
+            }
+
+            // Verificação de telefone existente
+            const recordsetTelefone = await this.queryDB(`SELECT * FROM ChargerHelp.Cliente WHERE telefone_cliente = '${telefone}'`);
+            if (recordsetTelefone.length > 0) {
+                throw new Error("Telefone já existe! Cadastro não realizado.");
+            }
+
+            // Inserção do novo cliente
+            await this.queryDB(`INSERT INTO ChargerHelp.Cliente (cpf_cliente, nome_cliente, telefone_cliente, email_cliente, senha_cliente)
+                                VALUES ('${cpf}', '${nome}', '${telefone}', '${email}', '${senha}')`);
+            
+            return "Novo cliente inserido com sucesso!";
+        } catch (error) {
+            if (error.number === 2627) {
+                // Se o erro for de violação de chave única (2627 é o código do SQL Server para essa violação)
+                throw new Error("Erro: Dados duplicados.");
+            } else {
+                throw new Error("Erro ao inserir cliente.");
+            }
+        }
+    }
+    
+    async buscarClientePorEmail(email_cliente) {
+        try {
+            const sql = `SELECT * FROM ChargerHelp.Cliente WHERE email_cliente = '${email_cliente}'`;
+            const recordset = await this.queryDB(sql);
+            return recordset;
+        } catch (error) {
+            throw new Error("Email de usuário não encontrado!");
+        }
     }
 
-    // inserirClienteNoBanco(nome,email,cpf,telefone,senha) {
-    //     return new Promise((resolve, reject) => {
-    //         const sql = `INSERT INTO ChargerHelp.Cliente (cpf_cliente,nome_cliente, telefone_cliente, email_cliente,senha_cliente)
-    //              VALUES ('${cpf}', '${nome}','${telefone}', '${email}','${senha}')`
-    //         this._bd.query(sql, function (erro, recordset) {
-    //             if (erro) {
-    //                 console.log(erro);
-    //                 return reject("CADASTRO DE CLIENTE FALHOU!");
-    //             }
-    //             resolve(recordset);
-    //         });
-    //     });
-    // }
-
-    buscarClientePorEmail(email_cliente) {
+    queryDB(sql) {
         return new Promise((resolve, reject) => {
-            var sql = `SELECT  * FROM ChargerHelp.Cliente WHERE email_cliente = '${email_cliente}'`;
-            this._bd.query(sql, function (erro, recordset) {
-                if (erro) {
-                    console.log(erro);
-                    return reject("EMAIL DE USUÁRIO NÃO ENCONTRADO!");
+            this._bd.query(sql, (error, recordset) => {
+                if (error) {
+                    console.log(error);
+                    reject(error);
                 }
                 resolve(recordset);
             });
         });
     }
-
 }
+
 module.exports = ClientesDao;
